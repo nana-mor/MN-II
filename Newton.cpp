78227af95f9de216422503762e831f9efc66004f
dani <421083671@pcpuma.acatlan.unam.mx>
2023-03-02 10:56:39 -0600

    Crear Funcion ElegirSistema

--- Newton.cpp
+++ Newton.cpp
@@ -6,30 +6,73 @@ using namespace std;
 typedef vector<double> Vector;
 typedef vector<Vector> Matriz;
 
-void Portada(){
+void Portada(){ //Se imprime información de la escuela y nombres de los integrantes del equipo
   
-  cout<<"UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO"<<endl;
-  cout<<"Facultad de Estudios Superiores Acatlán"<<endl;
-  cout<<"  Matemáticas Aplicadas y Computación"<<endl;
-  cout<<"         Métodos Numéricos II"<<endl;
+  cout<<"\n\nUNIVERSIDAD NACIONAL AUTONOMA DE MEXICO"<<endl;
+  cout<<"Facultad de Estudios Superiores Acatlan"<<endl;
+  cout<<"  Matematicas Aplicadas y Computacion"<<endl;
+  cout<<"         Metodos Numericos II"<<endl;
   cout<<"               Grupo 2403\n\n"<<endl;
-  cout<<"     Alonso López Carlos Arnulfo"<<endl;
+  cout<<"     Alonso Lopez Carlos Arnulfo"<<endl;
   cout<<"    Amador Navarro Dana Alejandra"<<endl;
   cout<<"      Rioja Romero Kevin Alexis"<<endl;
-  cout<<"      Vargas González José Luis\n\n"<<endl;
+  cout<<"      Vargas Gonzalez Jose Luis\n\n"<<endl;
   
+}
+
+int ElegirSistema(){ //Función que imprime los sistemas de ecuaciones e ingresa la elección por consola
+    int opcion;
+    cout<<"SISTEMAS DE ECUACIONES:"<<endl;
+  
+    cout<<"\n\tSistema 1:"<<endl;
+    cout<<"f1(x,y)=x^2+xy-10=0"<<endl;
+    cout<<"f2(x,y)=y+3xy^2-50=0"<<endl;
+    
+    cout<<"\n\tSistema 2:"<<endl;
+    cout<<"f1(x,y)=x^2+y^2-9=0"<<endl;
+    cout<<"f2(x,y)=-e^x-2y-3=0"<<endl;
+    
+    cout<<"\n\tSistema 3:"<<endl;
+    cout<<"f1(x,y,z)=2x^2-4x+y^2+3z^2+6z+z=0"<<endl;
+    cout<<"f2(x,y,z)=x^2+y^2-2y+2z^2-5=0"<<endl;
+    cout<<"f3(x,y,z)=3x^2-12x+y^2-3z^2+8=0"<<endl;
+    
+    cout<<"\n\tSistema 4:"<<endl;
+    cout<<"f1(x,y,z)=x^2-4x+y^2=0"<<endl;
+    cout<<"f2(x,y,z)=x^2-x-12-12y+1=0"<<endl;
+    cout<<"f3(x,y,z)=3x^2-12x+y^2-3z^2+8=0"<<endl;
+
+    // Se ingresa el sistema y se verifica que sea una entrada válida
+    while(true){ //try-catch continúa hasta que se reciba una entrada válida
+        try{
+            cout<<"\nIngresa el sistema que deseas resolver: ";
+            cin>>opcion; //Se ingresa la opción por consola
+            if(0 < opcion && opcion <5) break; //Se verifica que sea un entero en un intervalo [1,4]
+            else{
+                cout<<"\n----> No es una opcion valida. Vuelve a intentarlo.\n\n";
+                throw opcion;
+            }
+        }catch(...){ //En caso de error o si la entrada no se encuentra en el intervalo [1,4]
+            //Se descarta la entrada
+            cin.clear();
+            cin.ignore(100, '\n');
+        }
+    }
+
+    return opcion;
 }
 
 
 int main() {
-   
-  Portada();
-  while(true){
+    int sistema;
+    Portada();
+    while(true){
+        sistema = ElegirSistema();
+        cout<<sistema<<endl;
 
 
-
-    break;
-  }
+        break;
+    }
 
   
 }
