2813b3b6ce45a415a4fb611c02a19e6fd147fe18
dani <421083671@pcpuma.acatlan.unam.mx>
2023-03-02 10:43:57 -0600

    Crear Función Portada

--- Newton.cpp
+++ Newton.cpp
@@ -6,11 +6,30 @@ using namespace std;
 typedef vector<double> Vector;
 typedef vector<Vector> Matriz;
 
+void Portada(){
+  
+  cout<<"UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO"<<endl;
+  cout<<"Facultad de Estudios Superiores Acatlán"<<endl;
+  cout<<"  Matemáticas Aplicadas y Computación"<<endl;
+  cout<<"         Métodos Numéricos II"<<endl;
+  cout<<"               Grupo 2403\n\n"<<endl;
+  cout<<"     Alonso López Carlos Arnulfo"<<endl;
+  cout<<"    Amador Navarro Dana Alejandra"<<endl;
+  cout<<"      Rioja Romero Kevin Alexis"<<endl;
+  cout<<"      Vargas González José Luis\n\n"<<endl;
+  
+}
 
 
 int main() {
    
-  
+  Portada();
+  while(true){
+
+
+
+    break;
+  }
 
   
 }
